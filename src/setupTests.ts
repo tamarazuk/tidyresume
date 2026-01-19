8f247128e275189ec09387a8bee7eb3fa282ee83
Tamara Zuk <tamarazuk@gmail.com>
2026-01-19 16:02:46 -0500

    test: add global ResizeObserver mock

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -1,4 +1,11 @@
-import { expect } from 'vitest'
+import { expect, vi } from 'vitest'
 import * as matchers from '@testing-library/jest-dom/matchers'
 
 expect.extend(matchers)
+
+// Mock ResizeObserver
+global.ResizeObserver = class ResizeObserver {
+  observe = vi.fn()
+  unobserve = vi.fn()
+  disconnect = vi.fn()
+} as any
