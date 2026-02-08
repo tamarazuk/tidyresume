c9c1dccdf6d1f83630e999c95b6c66a67fbaf983
Tamara Zuk <tamarazuk@gmail.com>
2026-02-07 22:11:28 -0500

    style: align combobox story import order with project convention

--- src/components/ui/combobox.stories.tsx
+++ src/components/ui/combobox.stories.tsx
@@ -1,5 +1,5 @@
-import type { Meta, StoryObj } from '@storybook/react'
 import { useRef } from 'react'
+import type { Meta, StoryObj } from '@storybook/react'
 import {
   Combobox,
   ComboboxContent,
