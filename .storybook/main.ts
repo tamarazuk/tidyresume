6a0a7a032f27c0c2910b7a717838dfe8809227cb
Tamara Zuk <tamarazuk@gmail.com>
2026-02-07 21:51:31 -0500

    fix: make storybook addon capabilities explicit

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -2,10 +2,19 @@ import type { StorybookConfig } from '@storybook/nextjs'
 
 const config: StorybookConfig = {
   stories: ['../src/**/*.stories.@(ts|tsx)'],
-  addons: [],
+  addons: [
+    '@storybook/addon-docs',
+    '@storybook/addon-a11y',
+    '@storybook/addon-vitest',
+  ],
   framework: {
     name: '@storybook/nextjs',
     options: {},
+  },
+  features: {
+    controls: true,
+    interactions: true,
+    actions: true,
   },
   staticDirs: ['../public'],
 }
